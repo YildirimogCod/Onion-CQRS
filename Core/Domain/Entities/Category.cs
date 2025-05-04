328d3e47def973c7c5ab73b914a90b31ff55068b
yildirimogcod <yildirimogcod@gmail.com>
2025-05-04 17:06:58 +0300

    Entityler ve İlişkileri oluşturuldu.

---
+++ Core/Domain/Entities/Category.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Category:BaseEntity
+    {
+        public Category()
+        {
+            
+        }
+        public Category(int parentId, string name, int priorty)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priorty = priorty;
+        }
+        public required int ParentId { get; set; }
+
+        public required string Name { get; set; }    
+
+        public required int Priorty { get; set; }
+
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+    
+}
---
+++ Core/Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Product:BaseEntity
+    {
+       public required string Title { get; set; }
+       public required string Description { get; set; }
+
+       public required Brand BrandId { get; set; }
+       public required decimal Price { get; set; }
+
+        public required decimal Discount { get; set; }
+
+        public Brand Brand { get; set; }
+
+        public ICollection<Category> Categories { get; set; }
+
+    }
+}
