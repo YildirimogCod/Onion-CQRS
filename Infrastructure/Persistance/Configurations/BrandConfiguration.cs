fc7cf2e72cb04418ecc16377ad23dcecb9be979d
yildirimogcod <yildirimogcod@gmail.com>
2025-05-04 18:15:19 +0300

    DbContext ve Configurationslar yapıldı.

---
+++ Infrastructure/Persistance/Configurations/BrandConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Bogus;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistance.Configurations
+{
+    public class BrandConfiguration:IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(256);
+            Faker faker = new("tr");
+            Brand brand1 = new Brand
+            {
+                Id = 1,
+                Name = faker.Commerce.Department(),
+                CreatedAt = DateTime.UtcNow,
+                IsDeleted = false
+            };
+            Brand brand2 = new Brand
+            {
+                Id = 2,
+                Name = faker.Commerce.Department(),
+                CreatedAt = DateTime.UtcNow,
+                IsDeleted = false
+            };
+            Brand brand3 = new Brand
+            {
+                Id = 3,
+                Name = faker.Commerce.Department(),
+                CreatedAt = DateTime.UtcNow,
+                IsDeleted = true
+            };
+            builder.HasData(brand1, brand2, brand3);
+        }
+    }
+}
--- Presentation/Api/Program.cs
+++ Presentation/Api/Program.cs
@@ -1,3 +1,5 @@
+using Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +12,7 @@ var env = builder.Environment;
 builder.Configuration.SetBasePath(env.ContentRootPath)
     .AddJsonFile("appsettings.json",optional:false) 
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json",optional:true);
+builder.Services.AddPersistance(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
