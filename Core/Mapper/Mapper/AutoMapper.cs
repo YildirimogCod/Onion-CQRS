ec8e1f4904a811d4e6d8b6292a59b823311b03a4
yildirimogcod <yildirimogcod@gmail.com>
2025-05-23 01:54:03 +0300

    Automapper eklendi fakat henüz configure edilmedi.

---
+++ Core/Mapper/Mapper/AutoMapper.cs
@@ -0,0 +1,54 @@
+﻿using Application.Interfaces.AutoMapper;
+using AutoMapper;
+using AutoMapper.Internal;
+using IMapper = AutoMapper.IMapper;
+
+namespace Mapper.Mapper
+{
+    public class AutoMapper:Application.Interfaces.AutoMapper.IMapper
+    {
+        public static List<TypePair> typePairs = new();
+        private IMapper MapperContainer;
+
+        public TDestination Map<TDestination, TSource>(TSource source, string? ignore = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TDestination Map<TDestination, TSource>(IList<TSource> source, string? ignore = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TDestination Map<TDestination>(object source, string? ignore = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TDestination Map<TDestination>(IList<object> source, string? ignore = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Config<TDestination, TSource>(int depth = 5, string? ignore = null)
+        {
+            var typePair = new TypePair(typeof(TSource), typeof(TDestination));
+            if(typePairs.Any(a=>a.DestinationType == typePair.DestinationType && a.SourceType == typePair.SourceType)&& ignore is null)
+                return;
+            typePairs.Add(typePair);
+            var config = new MapperConfiguration(cfg =>
+            {
+                foreach (var item in typePairs)
+                {
+                    if (ignore is not null)
+                        cfg.CreateMap(item.SourceType, item.DestinationType).MaxDepth(depth)
+                            .ForMember(ignore, x => x.Ignore()).ReverseMap();
+                    else
+                        cfg.CreateMap(item.SourceType, item.DestinationType).MaxDepth(depth).ReverseMap();
+                }
+
+            });
+            MapperContainer = config.CreateMapper();
+        }
+    }
+}
